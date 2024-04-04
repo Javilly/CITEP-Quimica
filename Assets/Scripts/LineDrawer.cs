225dbb43549d1b861b9f61c20e17f9ef744af218
Javilly <javi.valda@gmail.com>
2024-04-04 15:34:39 -0300

    nuevas imagenes, fixes del armado de moleculas

--- Assets/Scripts/LineDrawer.cs
+++ Assets/Scripts/LineDrawer.cs
@@ -1,34 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LineDrawer : MonoBehaviour
 {
-    [Header("CorrectGeometry1")]
     [SerializeField]
-    private Transform[] geometry1Elements;
+    private GameObject linePrefab;
+
+    [SerializeField]
+    private Connection[] geometryElements;
+
+    [SerializeField]
+    private Sprite lineImage;
+
+    [SerializeField]
+    private RectTransform item;
+
+    [SerializeField]
+    private RectTransform item2;
 
     void Start()
     {
-        if (gameObject.name == "CorrectGeometry1")
+        // Vector3[] first = new Vector3[4];
+        // item.GetComponent<RectTransform>().GetWorldCorners(first);
+        // print(first[0]);
+
+        // Vector3[] second = new Vector3[4];
+        // item2.GetComponent<RectTransform>().GetWorldCorners(second);
+        // print(second[0]);
+
+        // GameObject newLine2 = Instantiate(linePrefab, new Vector3(0, 0, -6), Quaternion.identity);
+        // LineRenderer lineRenderer2 = newLine2.GetComponent<LineRenderer>();
+
+        // lineRenderer2.positionCount = 2;
+
+        // Vector3[] positionsArray = new Vector3[2];
+
+        // positionsArray[0] = new Vector3(first[0].x, first[0].y, -1);
+        // positionsArray[1] = new Vector3(second[0].x, second[0].y, -1);
+
+        // lineRenderer2.SetPositions(positionsArray);
+
+        for (int i = 0; i < geometryElements.Length; i++)
         {
-            // print("PEPE");
-            // GUIUtility.ScaleAroundPivot(
-            //     geometry1Elements[0].position,
-            //     geometry1Elements[1].position
-            // );
-            // GUI.Label(new Rect(200, 200, 256, 256), "");
+            GameObject newLine = Instantiate(linePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            newLine.transform.SetParent(transform);
+            LineRenderer lineRenderer = newLine.GetComponent<LineRenderer>();
 
-            // LineRenderer lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
-            // lineRenderer.startColor = Color.black;
-            // lineRenderer.endColor = Color.black;
-            // lineRenderer.startWidth = 0.01f;
-            // lineRenderer.endWidth = 0.01f;
-            // lineRenderer.positionCount = 2;
-            // lineRenderer.useWorldSpace = true;
-
-            // lineRenderer.SetPosition(0, geometry1Elements[0].position);
-            // lineRenderer.SetPosition(1, geometry1Elements[1].position);
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPositions(geometryElements[i].GetPositions());
         }
+    }
+}
+
+[System.Serializable]
+public class Connection
+{
+    [SerializeField]
+    public GameObject pointA;
+
+    [SerializeField]
+    public GameObject pointB;
+
+    public Vector3[] GetPositions()
+    {
+        Vector3[] positionsArray = new Vector3[2];
+        Vector3[] first = new Vector3[4];
+        pointA.GetComponent<RectTransform>().GetWorldCorners(first);
+        positionsArray[0] = new Vector3(first[0].x, first[0].y, -5);
+
+        Vector3[] second = new Vector3[4];
+        pointB.GetComponent<RectTransform>().GetWorldCorners(second);
+        positionsArray[1] = new Vector3(second[0].x, second[0].y, -5);
+
+        return positionsArray;
     }
 }
